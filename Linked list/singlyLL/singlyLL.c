16e5add10777f3cfb4bbdf02d45a2ba9f6c3d9a0
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-09-05 17:21:02 +0100

    Implementation and traversal of singly linked list

---
+++ Linked list/singlyLL/singlyLL.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  struct node
+  {                    // This is the structure of each node in the linkedlist
+    int data;          // variable of the datas that will be in each node
+    struct node *next; // struct node here is because struct node is the datatype of the next node address the next pointer will store
+  };
+
+  struct node *head, *firstnode, *newnode, *lastnode;
+  head = NULL;
+
+  firstnode = (struct node *)malloc(sizeof(struct node));
+  printf("Enter the first element: ");
+  scanf("%d", &firstnode->data);
+
+  firstnode->next = NULL;
+  head = firstnode;
+
+  newnode = (struct node *)malloc(sizeof(struct node));
+  printf("Enter the new element: ");
+  scanf("%d", &newnode->data);
+
+  newnode->next = NULL;
+  firstnode->next = newnode;
+
+  lastnode = (struct node *)malloc(sizeof(struct node));
+  printf("Enter the last element: ");
+  scanf("%d", &lastnode->data);
+
+  lastnode->next = NULL;
+  newnode->next = lastnode;
+
+  printf("%d", firstnode->data);
+
+  return 0;
+}
