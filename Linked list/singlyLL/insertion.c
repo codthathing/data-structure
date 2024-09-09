1769f37563871a21405b44ce722ec4e9fda705df
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-09-09 23:53:38 +0100

    Insertion in singly liked list

--- Linked list/singlyLL/insertion.c
+++ Linked list/singlyLL/insertion.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "singlyfunction.h"
 
-void main() {
-  struct node *temp = singlyTraversal();  
+struct node *insertBegin();
+struct node *insertEnd();
+struct node *insertAny();
+
+void main()
+{
+  struct node *temp = singlyTraversal();
+
+  struct node *insert = insertEnd(temp);
+
+  while (insert != NULL)
+  {
+    printf("%d \n", insert->data);
+    insert = insert->next;
+  }
+}
+
+struct node *insertBegin(struct node *temp)
+{
+  struct node *newnode;
+  newnode = (struct node *)malloc(sizeof(struct node));
+  printf("What value do you want to insert: ");
+  scanf("%d", &newnode->data);
+
+  newnode->next = temp;
+  temp = newnode;
+
+  return temp;
+}
+
+struct node *insertEnd(struct node *temp)
+{
+  struct node *newnode;
+  newnode = (struct node *)malloc(sizeof(struct node));
+  printf("What value do you want to insert: ");
+  scanf("%d", &newnode->data);
+  
+  newnode->next = NULL;
+
+  while (temp->next != NULL)
+  {
+    temp = temp->next;
+  }
+  temp->next = newnode;
+
+  return temp;
+}
+
+struct node *insertAny()
+{
 }
