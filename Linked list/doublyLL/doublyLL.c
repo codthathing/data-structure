f400defd0d4cde4dcb4a5fb6a8ab173540449034
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-09-14 22:49:16 +0100

    started doubly linked list

--- Linked list/doublyLL/doublyLL.c
+++ Linked list/doublyLL/doublyLL.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main()
 {
@@ -8,4 +9,35 @@ void main()
     int data;
     struct node *next;  
   };
+
+  struct node *head, *firstnode, *newnode, *lastnode, *temp;
+  head = NULL;
+
+  firstnode = (struct node*) malloc(sizeof(struct node));
+  printf("Enter first element: ");
+  scanf("%d", &firstnode->data);
+  firstnode->next = NULL;
+  firstnode->prev = NULL;
+  head = firstnode;
+
+  newnode = (struct node*) malloc(sizeof(struct node));
+  printf("Enter new element: ");
+  scanf("%d", &newnode->data);
+  newnode->next = NULL;
+  newnode->prev = firstnode;
+  firstnode->next = newnode;
+
+  lastnode = (struct node*) malloc(sizeof(struct node));
+  printf("Enter last element: ");
+  scanf("%d", &lastnode->data);
+  lastnode->next = NULL;
+  lastnode->prev = newnode;
+  newnode->next = lastnode;
+
+  temp = head;
+  while (temp != NULL)
+  {
+    printf("%d \n", temp->data);
+    temp = temp->next;
+  }
 }
